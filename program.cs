450a97bdd80139563cf8c97fbd42ae49e0c2592f
ChinYoong <49386569+ChinYoong@users.noreply.github.com>
2023-07-24 16:47:05 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,10 @@
+    public async Task PatchAsync<T>(string auth, string path, T view)
+    {
+        _client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(auth);
+
+        var json = JsonSerializer.Serialize(view, JsonSerialisationOptions.DefaultOptions);
+
+        var response = await _client.PatchAsync(BuildUri(path), new StringContent(json, Encoding.UTF8, "application/json"));
+
+        response.EnsureSuccessStatusCode();
+    }
